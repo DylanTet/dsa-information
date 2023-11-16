105b26b5278ac368dfb12ba8aac335a59c1567bd
Dylan <tettemerd@gmail.com>
2023-11-16 13:14:16 -0800

    added generate parentheses problem

---
+++ DSA/LeetCode-Problems/Stack/generate-parentheses.ts
@@ -0,0 +1,31 @@
+/*
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+*/
+
+function generateParenthesis(n: number): string[] {
+    const stack = new Array<string>;
+    const res = new Array<string>;
+
+    function backtrack(openN: number, closedN: number) {
+        if (openN === n && closedN === n) {
+            res.push(stack.join(''));
+            return;
+        }
+
+        if (openN < n) {
+            stack.push('(');
+            backtrack(openN + 1, closedN);
+            stack.pop();
+        }
+        console.log(stack);
+
+        if (closedN < openN) {
+            stack.push(')');
+            backtrack(openN, closedN + 1);
+            stack.pop();
+        }
+    }
+
+    backtrack(0, 0);
+    return res;
+};
